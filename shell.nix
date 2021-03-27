2e63c4fff7261896fba75befd12108569c74a967
Mikael Myyrä <mikael.myyrae@gmail.com>
2021-03-27 12:30:57 +0200

    Add niv for nixpkgs management

--- shell.nix
+++ shell.nix
@@ -1,16 +1,15 @@
-{ pkgs ? import (fetchTarball {
-  # nixos-20.03
-  url = "https://github.com/NixOS/nixpkgs-channels/archive/ab3adfe1c769c22b6629e59ea0ef88ec8ee4563f.tar.gz";
-  sha256 = "1m4wvrrcvif198ssqbdw897c8h84l0cy7q75lyfzdsz9khm1y2n1";
-}) {}
-}:
+{ sources ? import ./nix/sources.nix }:
 
 let
+  pkgs = import sources.nixpkgs {};
   jekyll_env = pkgs.bundlerEnv {
     name = "jekyll_env";
     gemdir = ./.;
   };
 in
 pkgs.mkShell {
-  buildInputs = [ jekyll_env ];
+  buildInputs = [
+    pkgs.niv
+    jekyll_env
+  ];
 }
