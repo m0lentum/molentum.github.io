4e08fc58c09d2aeeffa7b4a03e67e5af21ac2596
Mikael Myyrä <mikael.myyrae@gmail.com>
2025-04-19 14:12:35 +0300

    Update Zola and other Nix sources
    For some reason this broke latex
    specifically in cases that contain tildes,
    and needed a couple of config changes,
    but everything else seems to be fine

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
